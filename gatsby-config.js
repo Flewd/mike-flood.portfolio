edd48e8fea1f94fc814c3f82db43f10006aacdcb
Michael Flood <michael.flood@weplaydots.com>
2020-08-23 16:00:47 -0500

    initial portfolio project with minor edits

---
+++ gatsby-config.js
@@ -0,0 +1,60 @@
+require(`dotenv`).config({
+  path: `.env`,
+})
+
+const shouldAnalyseBundle = process.env.ANALYSE_BUNDLE
+
+module.exports = {
+  siteMetadata: {
+    siteTitleAlt: `Michael Flood's Game Development Portfolio`,
+  },
+  plugins: [
+    "gatsby-plugin-theme-ui",
+    {
+      resolve: `@lekoarts/gatsby-theme-emilia`,
+      // See the theme's README for all available options
+      options: {},
+    },
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: process.env.GOOGLE_ANALYTICS_ID,
+      },
+    },
+    `gatsby-plugin-sitemap`,
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `Michael Flood`,
+        short_name: `Mike Flood's Portfolio`,
+        description: `Michael Flood's Game Development Portfolio`,
+        start_url: `/`,
+        background_color: `#fff`,
+        theme_color: `#3182ce`,
+        display: `standalone`,
+        icons: [
+          {
+            src: `/android-chrome-192x192.png`,
+            sizes: `192x192`,
+            type: `image/png`,
+          },
+          {
+            src: `/android-chrome-512x512.png`,
+            sizes: `512x512`,
+            type: `image/png`,
+          },
+        ],
+      },
+    },
+    `gatsby-plugin-offline`,
+    `gatsby-plugin-netlify`,
+    shouldAnalyseBundle && {
+      resolve: `gatsby-plugin-webpack-bundle-analyser-v2`,
+      options: {
+        analyzerMode: `static`,
+        reportFilename: `_bundle.html`,
+        openAnalyzer: false,
+      },
+    },
+  ].filter(Boolean),
+}
